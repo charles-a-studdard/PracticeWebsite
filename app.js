ea396e033b42d5b87d4cd3476bc6d451740de512
cstuddar <cstuddar@calpoly.edu>
2021-11-08 14:08:52 -0500

    added button to change song

--- app.js
+++ app.js
@@ -1,11 +1,12 @@
 'use strict';
 
 const switcher = document.querySelector('.btn');
+var i = 0;
 
 switcher.addEventListener('click', function() {
     document.body.classList.toggle('dark-theme')
     document.body.classList.toggle('light-theme')
-
+    
     var className = document.body.className;
     if(className == "light-theme") {
         this.textContent = "Dark";
@@ -15,14 +16,16 @@ switcher.addEventListener('click', function() {
     }
     console.log('current class name: ' + className);
 
-
-
 });
 
 const songSwitcher = document.querySelector('.changeSongBtn');
 
 songSwitcher.addEventListener('click', function() {
-    document.body.iframe.srcList.toggle('https://open.spotify.com/embed/track/3KENEwjmiRoWoW3HTE9JNb?utm_source=generator')
+    var srcs = ["https://open.spotify.com/embed/track/3KENEwjmiRoWoW3HTE9JNb?utm_source=generator","https://open.spotify.com/embed/track/1NaDK3atogQEn9glK5fYuy?utm_source=generator"]
+    
+    document.getElementById("currentSong").src = srcs[i%2];
+    i++;
+    console.log('switched');
 });
 
 
